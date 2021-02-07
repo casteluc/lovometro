ffbcca4e4282f550d39ab8e74fb3c0a8198dd812
Lucas Torres <lucasrib.ia@gmail.com>
2021-02-07 01:09:24 -0300

    loading assets and initial page structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import Lovometro from './pages/Lovometro'
+
 import './App.css';
 
 function App() {
   return (
     <div className="app">
-
+        <Lovometro/>
     </div>
   );
 }
---
+++ src/pages/Lovometro/index.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import Heart from '../../assets/heart.png'
+
+function Lovometro() {
+    return (
+        <main>
+            <div>
+                <div>
+                    <h1>Lovometro</h1>
+                    <h2>Descubra o quanto você e a pessoa amada se combinam</h2>
+                </div>
+                <form>
+                    <input type="text" name="fname"/>
+                    <input type="text" name="sname"/>
+                    <input type="submit" value="calcular"/>
+                </form>
+            </div>
+            <div>
+                <img src={Heart} alt="Heart"/>
+            </div>
+        </main>
+    )
+}
+
+export default Lovometro
